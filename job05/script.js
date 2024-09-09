ff5e437e1074e7594bae8b4174651b302547bfeb
Dounayha <dounayha05@gmail.com>
2024-09-09 11:24:24 +0200

    job05

---
+++ job05/script.js
@@ -0,0 +1,13 @@
+function afficherjourssemaines() {
+    const jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+    // Boucle for pour afficher chaque jour de la semaine
+    for (let compteur = 0; compteur < jourssemaines.length; compteur++) {
+        console.log(jourssemaines[compteur]); 
+    }
+}
+
+// Appel de la fonction pour tester son fonctionnement
+afficherjourssemaines();
+    
+
